e90ca28e2024fed89dfa62c36b24d1984bf3fe21
mytlx <mytlx1466@gmail.com>
2025-05-11 23:26:17 +0800

    go-sqlite-demo add model-table name mapping

--- go-web-template/internal/model/user.go
+++ go-web-template/internal/model/user.go
@@ -1,5 +1,17 @@
 package model
 
+// User 建表语句
+// CREATE TABLE `user` (
+//
+//	`id` bigint NOT NULL AUTO_INCREMENT,
+//	`name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+//	`gender` int DEFAULT NULL COMMENT '0：女，1：男',
+//	`age` int DEFAULT NULL,
+//	`email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+//	`address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+//	PRIMARY KEY (`id`)
+//
+// ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 type User struct {
 	ID      int64  `gorm:"primaryKey" json:"id"`
 	Name    string `gorm:"column:name" binding:"required" json:"name"`
@@ -7,4 +19,9 @@ type User struct {
 	Age     int    `gorm:"column:age" binding:"gte=0,lte=120" json:"age"`
 	Email   string `gorm:"column:email" binding:"required,email" json:"email"`
 	Address string `gorm:"column:address" json:"address"`
+}
+
+// TableName 指定表名
+func (User) TableName() string {
+	return "user"
 }
